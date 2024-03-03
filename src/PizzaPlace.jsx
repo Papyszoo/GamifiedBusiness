027dc3fca79531cf6f134355482002c916b379d4
Patryk Kowalczyk <papyszoo@gmail.com>
2024-03-03 21:47:15 +0100

    added stars environment map

--- src/PizzaPlace.jsx
+++ src/PizzaPlace.jsx
@@ -1,4 +1,4 @@
-import { Environment, Float } from "@react-three/drei";
+import { Environment, Float, useEnvironment } from "@react-three/drei";
 import { useThree, extend, useLoader } from "@react-three/fiber";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as THREE from "three";
@@ -7,31 +7,21 @@ extend({ OrbitControls });
 
 export default function PizzaPlace() {
     const { camera, gl } = useThree();
-    const colorMap = useLoader(
-        THREE.TextureLoader,
-        "https://cdn.polyhaven.com/asset_img/primary/aerial_beach_02.png?height=4096"
-    );
+
+    const envMap = useEnvironment({
+        files: "/envmap/stars.hdr",
+    });
 
     return (
         <>
             <orbitControls args={[camera, gl.domElement]} />
             <Environment
+                map={envMap}
                 background
                 near={1}
                 far={1000}
-                resolution={256}
                 frames={Infinity}
-            >
-                <Float>
-                    <mesh scale={100}>
-                        <sphereGeometry args={[1, 64, 64]} />
-                        <meshBasicMaterial
-                            map={colorMap}
-                            side={THREE.BackSide}
-                        />
-                    </mesh>
-                </Float>
-            </Environment>
+            ></Environment>
 
             <directionalLight position={[1, 2, 3]} intensity={4.5} />
             <ambientLight intensity={1.5} />
