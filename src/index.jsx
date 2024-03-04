cdc4efb15f40aef75fe1c313b3703d62809e15d1
Patryk Kowalczyk <papyszoo@gmail.com>
2024-03-05 00:19:49 +0100

    added three-stdlib package and r3f-perf to canvas

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,13 @@ import "./style.css";
 import ReactDOM from "react-dom/client";
 import { Canvas } from "@react-three/fiber";
 import PizzaPlace from "./PizzaPlace/PizzaPlace";
+import { Perf } from "r3f-perf";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
 root.render(
     <Canvas camera={{ position: [0, 0, 10] }}>
+        <Perf position="top-left" />
         <PizzaPlace />
     </Canvas>
 );
